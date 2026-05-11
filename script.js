a750dbdf97a55777d94d0a290c425cd75da4685a
Haenjeo <222016381+Haenjeo@users.noreply.github.com>
2026-05-11 15:54:46 +0700

    feat: add new function playRound()
    Create new function playRound() giving log about result of the rond wheter human win lose or draw.

--- script.js
+++ script.js
@@ -21,3 +21,25 @@ let humanScore = 0;
 // Create new variable get computerScore
 // Initialize value with 0
 let computerScore = 0;
+
+// Make playRound function
+function playRound(humanChoice, computerChoice) {
+    let human = humanChoice.toUpperCase();
+    let computer = computerChoice;
+
+    if (human === "ROCK" && computer === "PAPER") {
+        console.log("You lose! Paper beats Rock");
+    } else if (human === "PAPER" && computer === "SCISSOR") {
+        console.log("You lose! Scissor beats Paper");
+    } else if (human === "SCISSOR" && computer === "ROCK") {
+        console.log("You lose! Rock beats Scissor");
+    } else if (human === "PAPER" && computer === "ROCK") {
+        console.log("You win! Paper beats Rock");
+    } else if (human === "SCISSOR" && computer === "PAPER") {
+        console.log("You win! Scissor beats Paper");
+    } else if (human === "ROCK" && computer === "SCISSOR") {
+        console.log("You win! Rock beats Scissor");
+    } else {
+        console.log("You draw!");
+    }
+}

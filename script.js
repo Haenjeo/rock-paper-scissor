2d3ef04ae723be10dd2b3a219877c504f7b90650
Haenjeo <222016381+Haenjeo@users.noreply.github.com>
2026-05-11 15:27:16 +0700

    feat: add getComputerChoice function
    Adding arrow function on getComputerChoice variable, returning Math.random() [number 0 - 1]

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Hello, World!");
+
+// getComputerChoice
+// create variable computer-choice
+// assign value to varaible computer-choice
+// return the value
+
+ let getComputerChoice = () => Math.random();

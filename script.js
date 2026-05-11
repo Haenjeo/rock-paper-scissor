e077e7ba9fbf85150deba86c3b0ecf52bf142d25
Haenjeo <222016381+Haenjeo@users.noreply.github.com>
2026-05-11 15:39:33 +0700

    feat: create humanScore and computerScore
    Creating new humanScore and computerScore variables and initialize it to 0.

--- script.js
+++ script.js
@@ -13,3 +13,11 @@ console.log("Hello, World!");
 // return the value
 
  let getHumanChoice = (input) => prompt("Choose your Rock, Paper, Scissor",input);
+
+// Create new variable get humanScore
+// Initialize value with 0
+let humanScore = 0;
+
+// Create new variable get computerScore
+// Initialize value with 0
+let computerScore = 0;
